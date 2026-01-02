13ecfcb426599f7ad4e141e5423da28b3c979616
fajar <fazarwigunah@gmail.com>
2026-01-02 15:26:26 +0700

    menambahkan image

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1>Tentang Perusahaan:</h1>
+      <p>
+        Kursus Hi-five dibentuk pada tahun 2019 untuk menjembatani kesenjangan antara pengetahuan akademik dan keterampilan praktis yang dibutuhkan di dunia kerja. Kami berfokus pada tiga pilar utama: kurikulum berbasis praktik, instruktur
+        profesional, dan lingkungan belajar yang mendukung. Kami percaya bahwa setiap orang memiliki potensi besar yang hanya perlu diasah dengan bimbingan yang tepat
+      </p>
+    </div>
+  );
+};
+
+export default About;
